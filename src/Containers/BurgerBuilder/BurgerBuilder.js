2e26b7761b42a091c2c711b6c977930729478277
kagai <shaddy.mwangi@gmail.com>
2019-08-06 07:08:50 +0300

    WIP:burger componenent added

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger'
 
 export default class BurgerBuilder extends Component {
+   
+    state ={
+        ingredient:{
+            salad :1,
+            bacon :1,
+            cheese :2,
+            meat:2
+
+        }
+    }
+
     render() {
         return (
            <Aux>
-            <div>Burger</div>
+            <Burger ingredient ={this.state.ingredient}/>
             <div>Build Controls</div>
            </Aux>
         );
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+const transformedIngredients = Object.keys(props.ingredients)
+.map(igKey =>{
+    return [...Array(props.ingredients(igKey))].map(())
+})
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type ="bread-top" />
+            <BurgerIngredient type ="cheese" />
+            <BurgerIngredient type ="meat" />
+            <BurgerIngredient type ="bread-bottom" />
+        </div>
+      );
+}
+ 
+export default burger;
