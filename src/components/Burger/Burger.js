9bbe1ad0e81e1150c9b24656af57f1a94798ccad
kagai <shaddy.mwangi@gmail.com>
2019-08-07 08:59:51 +0300

    added the burger builder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,7 +6,7 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
 const transformedIngredients = Object.keys(props.ingredients)
 .map(igKey =>{
-    return [...Array(props.ingredients(igKey))].map(())
+    return [...Array(props.ingredients(igKey))].map((_,))
 })
 
     return (
